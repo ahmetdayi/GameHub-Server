bbb0abf34d66621f0318f2caf0e6110ca9c75110
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2022-11-19 20:41:23 +0300

    security and validation

--- src/main/java/com/software/gameHub/core/constant/Constant.java
+++ src/main/java/com/software/gameHub/core/constant/Constant.java
@@ -10,7 +10,7 @@ public class Constant {
     public static final String WALLET_ID_DOES_NOT_EXIST = "Wallet Id Doesn't exist";
     public static final String COMMENT_ID_DOES_NOT_EXIST = "Comment Id Doesn't Exist";
 
-    public static final String GAME_ALREADY_EXISTS_IN_BASKET = "Game Already Exists In Basket";
+    public static final String GAME_ALREADY_EXISTS_IN_BASKET = "Game Already Exists In Basketa";
     public static final String IMAGE_NOT_FOUND = "Image Not Found";
     public static final String MAX_IMAGE_LESS_THAN_SIX = "Max Image Less Than Six";
     public static final String TOKEN_NOT_VALID = "Token Not Valid";
