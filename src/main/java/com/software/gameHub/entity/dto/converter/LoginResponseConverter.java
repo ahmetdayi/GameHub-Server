9f1f91249cf923c542776eb63108e5909f29b393
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2022-11-28 14:06:58 +0300

    rearrange

--- src/main/java/com/software/gameHub/entity/dto/converter/LoginResponseConverter.java
+++ src/main/java/com/software/gameHub/entity/dto/converter/LoginResponseConverter.java
@@ -1,15 +1,20 @@
 package com.software.gameHub.entity.dto.converter;
 
+import com.software.gameHub.entity.dto.CustomerDto;
 import com.software.gameHub.entity.dto.LoginResponse;
+
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class LoginResponseConverter {
 
+    
 
-    public LoginResponse convert(String token){
-        return new LoginResponse(token);
+    public LoginResponse convert(String token,CustomerDto customer){
+        return new LoginResponse(token,customer);
 
     }
 }
