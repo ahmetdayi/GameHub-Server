7410de43b6529c2b474a4775273c29562bfcd7f7
batikanyeni <58271721+batikanyeni@users.noreply.github.com>
2022-11-29 20:51:16 +0300

    arrange

--- src/main/java/com/software/gameHub/entity/dto/LibraryDto.java
+++ src/main/java/com/software/gameHub/entity/dto/LibraryDto.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,5 +14,5 @@ public class LibraryDto {
 
     private int libraryId;
 
-    private BuyDto buy;
+    private List<BuyDto> buy;
 }
--- src/main/java/com/software/gameHub/repository/BuyDao.java
+++ src/main/java/com/software/gameHub/repository/BuyDao.java
@@ -3,5 +3,7 @@ package com.software.gameHub.repository;
 import com.software.gameHub.entity.Buy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BuyDao extends JpaRepository<Buy,Integer> {
 }
